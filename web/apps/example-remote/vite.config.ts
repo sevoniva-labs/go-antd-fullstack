8991c5b6056e8e49b9aece8208ddf79a31d121bf
Carson <chuncheng.carson@gmail.com>
2026-08-17 16:14:49 +0800

    perf(web): split production bundles with Rolldown

--- web/apps/example-remote/vite.config.ts
+++ web/apps/example-remote/vite.config.ts
@@ -34,4 +34,34 @@ export default defineConfig({
       'X-Content-Type-Options': 'nosniff',
     },
   },
+  build: {
+    sourcemap: false,
+    chunkSizeWarningLimit: 500,
+    rolldownOptions: {
+      output: {
+        strictExecutionOrder: true,
+        codeSplitting: {
+          groups: [
+            {
+              name: 'react-runtime',
+              test: /node_modules[\\/](?:react|react-dom|scheduler)(?:[\\/]|$)/,
+              priority: 30,
+            },
+            {
+              name: 'ant-design',
+              test: /node_modules[\\/](?:antd|@ant-design[\\/]|@rc-component[\\/]|rc-[^\\/]+)(?:[\\/]|$)/,
+              maxSize: 400 * 1024,
+              priority: 20,
+            },
+            {
+              name: 'vendor',
+              test: /node_modules/,
+              maxSize: 350 * 1024,
+              priority: 10,
+            },
+          ],
+        },
+      },
+    },
+  },
 });
--- web/apps/shell/vite.config.ts
+++ web/apps/shell/vite.config.ts
@@ -16,10 +16,47 @@ export default defineConfig({
       },
     },
   },
-  build: {
-    outDir: '../../dist',
-    emptyOutDir: true,
-    sourcemap: false,
-    chunkSizeWarningLimit: 1200,
-  },
-})
+    build: {
+      outDir: '../../dist',
+      emptyOutDir: true,
+      sourcemap: false,
+      chunkSizeWarningLimit: 500,
+      rolldownOptions: {
+        output: {
+          strictExecutionOrder: true,
+          codeSplitting: {
+            groups: [
+              {
+                name: 'react-runtime',
+                test: /node_modules[\\/](?:react|react-dom|scheduler)(?:[\\/]|$)/,
+                priority: 50,
+              },
+              {
+                name: 'ant-design-pro',
+                test: /node_modules[\\/]@ant-design[\\/]pro-/,
+                maxSize: 400 * 1024,
+                priority: 45,
+              },
+              {
+                name: 'ant-design',
+                test: /node_modules[\\/](?:antd|@ant-design[\\/]|@rc-component[\\/]|rc-[^\\/]+)(?:[\\/]|$)/,
+                maxSize: 400 * 1024,
+                priority: 40,
+              },
+              {
+                name: 'tanstack',
+                test: /node_modules[\\/]@tanstack[\\/]/,
+                priority: 35,
+              },
+              {
+                name: 'vendor',
+                test: /node_modules/,
+                maxSize: 350 * 1024,
+                priority: 10,
+              },
+            ],
+          },
+        },
+      },
+    },
+  })
